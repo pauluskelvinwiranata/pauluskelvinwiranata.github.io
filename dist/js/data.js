5bcef74504a02d7bec56ab382e0835171618e045
Paulus <you@example.com>
2023-08-18 08:54:58 +0700

    update

---
+++ dist/js/data.js
@@ -0,0 +1,47 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(xhttp.responseText);
+
+    data.simpleprojects.forEach(function (e) {
+      document.getElementById("simpleprojects").innerHTML += `
+            <div class="card-projects">
+            <img src="${e.image}" />
+            <div class="desc-projects">
+              <h1>${e.title}</h1>
+              <div class="tech-stack">
+                <i class="fa-solid fa-circle"></i>
+                <p>${e.type}</p>
+              </div>
+              <div class="cta">
+                <a href="${e.code}" target="_blank"><button><i class="fa-solid fa-code"></i></button></a>
+                <a href="${e.demo}" target="_blank"><button><i class="fa-solid fa-eye"></i></button></a>
+              </div>
+            </div>
+          </div>
+        `;
+    });
+
+    data.complexprojects.forEach(function (e) {
+      document.getElementById("complexprojects").innerHTML += `
+            <div class="card-projects">
+            <img src="${e.image}" />
+            <div class="desc-projects">
+              <h1>${e.title}</h1>
+              <div class="tech-stack">
+                <i class="fa-solid fa-circle"></i>
+                <p>${e.type}</p>
+              </div>
+              <div class="cta">
+                <a href=""><button disabled><i class="fa-solid fa-code"></i></button></a>
+                <a href=""><button disabled><i class="fa-solid fa-eye"></i></button></a>
+              </div>
+            </div>
+          </div>
+        `;
+    });
+  }
+};
+
+xhttp.open("GET", "dist/js/data.json", true);
+xhttp.send();
